d8a9e210ba8086e50bcd7659fac069912a8ba6f4
Richard Priestley <r.priestley@comanduk.co.uk>
2018-07-09 19:00:40 +0100

    Added postTest to protractor plugin to check for unexpected and unreceived requests

--- src/protractor/MockHttpServerPlugin.ts
+++ src/protractor/MockHttpServerPlugin.ts
@@ -12,8 +12,22 @@ const plugin: ProtractorPlugin = {
       port: 3000,
     };
     return server.start(config)
-      .then(() => console.log(`Started mock server on http:localhost:${config.port}`))
-      .catch((err) => console.error("Failed to start mock server", err));
+      .catch((err) => console.error(`Failed to start mock server on port: ${config.port}`, err));
+  },
+
+  postTest(): void {
+    try {
+      const hasUnexpectedRequests: boolean = server.getRequests().length > 0;
+      const hasNotReceivedRequests: boolean = server.getResponses().length > 0;
+      if (hasUnexpectedRequests || hasNotReceivedRequests) {
+        const requestUrls = server.getRequests().map((req) => req.url).join(", ");
+        const responseUrls = server.getResponses().map((res) => res.expression).join(", ");
+        throw new Error(`Received unexpected request(s): [${requestUrls}] and ` +
+          `did not receive request(s) for: [${responseUrls}]`);
+      }
+    } finally {
+      server.clear();
+    }
   },
 };
 
